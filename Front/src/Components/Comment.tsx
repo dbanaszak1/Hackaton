fdabd3aa5b588a54f72e2d14a43f7307a85204e3
Michal <michalluczak4a@gmail.com>
2024-06-16 01:07:35 +0200

    comments under forum post

---
+++ Front/src/Components/Comment.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+interface CommentProps {
+    comment: string;
+    creator: CreatorProps
+}
+
+interface CreatorProps {
+    completedTests: [],
+    forumPoints: number,
+    id: string,
+    isPremium: boolean,
+    name: string,
+    testPoints: number
+}
+
+const Comment = ({comment, creator}: CommentProps) => {
+    return (
+        <div className="bg-white p-6 rounded-lg shadow-md">
+            <h1 className="text-3xl font-bold mb-4">{comment}</h1>
+            <p className="text-gray-700 mb-4">{creator.name}</p>
+            <div className="flex items-center justify-between">
+                <div>
+                    <h2 className="text-lg font-semibold">Category: {comment}</h2>
+                    <h2 className="text-lg font-semibold">Subcategory: {comment}</h2>
+                    <p className="text-gray-600">Status: {comment}</p>
+                </div>
+                <div>
+                    {<><p className="text-gray-600">Posted by: {comment}</p><p className="text-gray-600">Forum
+                        Points: {comment}</p></>}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Comment
--- Front/src/Pages/SinglePost.tsx
+++ Front/src/Pages/SinglePost.tsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import {useParams} from 'react-router-dom';
 import axios from 'axios';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
+import Comment from '../Components/Comment'
+import ForumQuestion from "../Components/ForumQuestion.tsx";
 
 interface Post {
   category: string;
@@ -16,16 +18,26 @@ interface Post {
 
 const SinglePost = () => {
   const { id } = useParams();
-  const [post, setPost] = useState<Post[]>([]);
+  const [post, setPost] = useState<Post>();
+  const [comments, setComments] = useState<Comment[]>([]);
 
   useEffect(() => {
-    axios.get(`http://localhost:5000/post/${id}`)
+    axios.get(`http://localhost:5000/post/id/${id}`)
       .then(response => {
+        console.log(response.data)
         setPost(response.data.post);
       })
       .catch(error => {
         console.error("Error fetching post:", error);
       });
+    axios.get(`http://localhost:5000/post/${id}/comments`)
+        .then(response => {
+          console.log(response.data)
+          setComments(response.data.comments);
+        })
+        .catch(error => {
+          console.error("Error fetching post:", error);
+        });
   }, [id]);
 
   if (!post) {
@@ -35,20 +47,31 @@ const SinglePost = () => {
   return (
     <>
       <Navbar user="" />
-      <div className="container mx-auto p-4 max-w-[1240px] mt-10">
+      <div className="container mx-auto p-4 max-w-[1240px] mt-40">
         <div className="bg-white p-6 rounded-lg shadow-md">
-          <h1 className="text-3xl font-bold mb-4">{post[0].title}</h1>
-          <p className="text-gray-700 mb-4">{post[0].content}</p>
+          <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
+          <p className="text-gray-700 mb-4">{post.content}</p>
           <div className="flex items-center justify-between">
             <div>
-              <h2 className="text-lg font-semibold">Category: {post[0].category}</h2>
-              <h2 className="text-lg font-semibold">Subcategory: {post[0].subcategory}</h2>
-              <p className="text-gray-600">Status: {post[0].status}</p>
+              <h2 className="text-lg font-semibold">Category: {post.category}</h2>
+              <h2 className="text-lg font-semibold">Subcategory: {post.subcategory}</h2>
+              <p className="text-gray-600">Status: {post.status}</p>
             </div>
-            <div>
-              <p className="text-gray-600">Posted by: {post[0].creator.name}</p>
-              <p className="text-gray-600">Forum Points: {post[0].creator.forumPoints}</p>
-            </div>
+            {comments
+            .map((comment, index) => (
+              <Comment
+                  key={index}
+                  comment={comment.comment}
+                  creator={
+                    completedTests = comment.creator.completedTests,
+                    forumPoints = comment.creator.forumPoints,
+                    id = comment.creator.id,
+                    isPremium = comment.creator.isPremium,
+                    name = comment.creator.name,
+                    testPoints = comment.creator.testPoints
+                  }
+              />
+            ))}
           </div>
         </div>
       </div>
